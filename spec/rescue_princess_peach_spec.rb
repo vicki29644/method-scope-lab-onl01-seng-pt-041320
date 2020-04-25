485f58af1227594cb27f398098d45f1bc4b8a024
Victoria Renee Mowbray <vicki29644@icloud.com>
2020-04-25 01:03:30 +0000

    Automatically backed up by Learn

--- spec/rescue_princess_peach_spec.rb
+++ spec/rescue_princess_peach_spec.rb
@@ -3,4 +3,3 @@ require "spec_helper"
 describe "rescue_princess_peach" 
   "outputs a message and sets a variable,status, that is not available outside of this method"
   "Hooray! Mario has rescued Princess Peach"
-end
